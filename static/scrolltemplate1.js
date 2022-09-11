7400c4b8dabbdcad622221c7f9436385e7fdd8a2
POINTEL Jérémy <64093280+jeremy91510@users.noreply.github.com>
2022-09-11 22:56:14 +0200

    Add files via upload

--- static/scrolltemplate1.js
+++ static/scrolltemplate1.js
@@ -9,7 +9,7 @@ const frameCount = 165;
 const currentFrame = index => (
  
 
-`static/frame (${index}).jpg`
+`https://jeremypointel.fr/static/frame (${index}).jpg`
 //`static/machin${index}.jpg`
  // `https://www.apple.com/105/media/us/airpods-pro/2019/1299e2f5_9206_4470_b28e_08307a42f19b/anim/sequence/large/01-hero-lightpass/${index.toString().padStart(4, '0')}.jpg`
 )
